c5dc9853c1cc1cf3c527ebedb2671c462f538458
romaric roussel <romaric.roussel@outlook.fr>
2018-12-06 17:42:28 +0100

    init

--- src/main/lp/iem/tennis/TennisMatch.java
+++ src/main/lp/iem/tennis/TennisMatch.java
@@ -2,16 +2,48 @@ package lp.iem.tennis;
 
 public class TennisMatch {
 
-    Player player1;
-    Player player2;
-    MatchType matchType;
-    boolean tieBreakInLastSet;
+    private Player player1;
+    private Player player2;
+    private MatchType matchType;
+    private boolean tieBreakInLastSet;
+    private boolean isFinished;
+    private int currentSet;
+
 
     public TennisMatch(Player player1, Player player2, MatchType matchType, boolean tieBreakInLastSet) {
         this.player1 = player1;
         this.player2 = player2;
         this.matchType = matchType;
         this.tieBreakInLastSet = tieBreakInLastSet;
+    }
+    public void updateWithPointWonBy(Player player){
+
+    }
+    public String pointsForPlayer(Player player){
+        return "";
+    }
+    public int gamesInCurrentSetForPlayer(Player player){
+        return 1;
+    }
+    public int gamesInSetForPlayer(int setNumber, Player player){
+        return 1;
+    }
+
+    public int currentSetNumber(){
+        return currentSet;
+    }
+
+    public void setCurrentSetNumber(int currentSet){
+        this.currentSet = currentSet;
+    }
+
+
+    public boolean isFinished(){
+        return isFinished;
+    }
+
+    public void setFinished(boolean finishedMatch) {
+        isFinished = finishedMatch;
     }
 
     public Player getPlayer1() {
--- src/test/lp/iem/tennis/TennisMatchTest.java
+++ src/test/lp/iem/tennis/TennisMatchTest.java
@@ -1,16 +1,41 @@
 package lp.iem.tennis;
 
-import org.junit.Test;
-
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import java.util.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TennisMatchTest {
 
-    Player player1 = new Player("Romaric");
-    Player player2 = new Player("Maxime");
-    MatchType bo3 = new MatchType(MatchType.BEST_OF_THREE);
-    MatchType bo5 = new MatchType(MatchType.BEST_OF_FIVE);
-    boolean tieBreakInLastSet = false;
+    private Player player1 = new Player("Romaric");
+    private Player player2 = new Player("Maxime");
+    private MatchType bo3 = MatchType.BEST_OF_THREE;
+    private MatchType bo5 = MatchType.BEST_OF_FIVE;
+    private boolean tieBreakInLastSet = false;
+
+    private TennisMatch tennisMatch1  = new TennisMatch(player1,player2,bo3,false);
+    private TennisMatch tennisMatch2  = new TennisMatch(player1,player2,bo5,false);
 
 
+    @Before
+    public void init(){
+        tennisMatch1.setFinished(true);
+        tennisMatch2.setFinished(false);
+        tennisMatch2.setCurrentSetNumber(1);
+    }
+
+    @Test
+    public void isFinished() {
+        assertTrue(tennisMatch1.isFinished());
+        assertFalse(tennisMatch2.isFinished());
+
+    }
+
+
+    @Test
+    public void currentSetNumber() {
+        assertThat(tennisMatch2.currentSetNumber(), is(1));
+    }
 }
